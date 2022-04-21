4e93e8bd140623fd20534edcf7044c58bce1e5ba
Aleksandar Rusakov <aleksandar.rusakov@rewardgateway.com>
2022-04-22 00:54:21 +0300

    Install font if not present

--- scripts/fetch_font.sh
+++ scripts/fetch_font.sh
@@ -5,7 +5,7 @@ if [[ $(brew tap | grep -q homebrew/cask-fonts) -eq 1 ]]; then
     brew tap homebrew/cask-fonts
 fi
 
-if [[ $(brew list --cask | grep -q font-jetbrains-mono) -eq 0 ]]; then
+if [[ $(brew list --cask | grep -q font-jetbrains-mono) -eq 1 ]]; then
     printf "\033[0;92m==> %s\n\033[0m" "Installing Jetbrains Mono font"
     brew install --cask font-jetbrains-mono
 else
