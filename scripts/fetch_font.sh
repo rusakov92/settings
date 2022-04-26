ce6473248be49f2b51353a939f0e8d44c3659bd2
Aleksandar Rusakov <aleksandar.rusakov@rewardgateway.com>
2022-04-26 12:46:08 +0300

    Fixing fetch font script

--- scripts/fetch_font.sh
+++ scripts/fetch_font.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-if [[ $(brew tap | grep -q homebrew/cask-fonts) -eq 1 ]]; then
+brew tap | grep -q homebrew/cask-fonts
+if [ $? -eq 1 ]; then
     printf "\033[0;92m==> %s\n\033[0m" "Taping homebrew/cask-fonts"
     brew tap homebrew/cask-fonts
 fi
 
-if [[ $(brew list --cask | grep -q font-jetbrains-mono) -eq 1 ]]; then
+brew list --cask | grep -q font-jetbrains-mono
+if [ $? -eq 1 ]; then
     printf "\033[0;92m==> %s\n\033[0m" "Installing Jetbrains Mono font"
     brew install --cask font-jetbrains-mono
 else
